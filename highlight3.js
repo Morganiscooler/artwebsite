249fb87ab5ebbd926f8a345182e1c828be0f8732
Morgan C <morgancuiuwu@gmail.com>
2024-07-20 00:22:17 -0400

    updated index

--- highlight3.js
+++ highlight3.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
         document.body.style.setProperty("--otherColor", 'hsl('+hue+',100%,35%)');
         hue+=5;
         if (hue > 345) {
-            hue = 201;
+            hue = 201; 
         }
         requestAnimationFrame(loop);
     };
